e0fdc527f5678ead76872d56842aafbaa748cb0b
Tim Nelson <tim.nelson@droneup.com>
2022-11-28 15:30:45 -0600

    create script for setting up the pr build which will generate a new base image if one doesn't exist

---
+++ tools/l4t/setup-pr-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+uname -r
+sudo apt-get install qemu binfmt-support qemu-user-static
+
+build_opts="--force-rm=true $@"
+
+BASE_IMAGE="airmapreg.azurecr.io/platform-sdk.l4t.base:3"
+
+cd "${HERE}/../.."
+
+docker run --platform=linux/amd64 --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# build base image if it does not yet exist
+if [[ -z "$(docker images -q "${BASE_IMAGE}")" ]]; then
+    docker build --file "${HERE}/../../docker/l4t/l4t.base" --tag "${BASE_IMAGE}" .
+fi
