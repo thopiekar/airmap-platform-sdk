9cce71bc190c63a1926db1d831528696ba38c71e
Tim Nelson <tim.nelson@droneup.com>
2023-02-23 17:51:52 -0600

    resize string

--- src/airmap/rest/telemetry.cpp
+++ src/airmap/rest/telemetry.cpp
@@ -109,7 +109,8 @@ std::string airmap::rest::detail::OpenSSLAES256Encryptor::create_shared_secret()
 std::string airmap::rest::detail::OpenSSLAES256Encryptor::encrypt(const std::string& message, const std::string& key,
                                                                   const std::string& iv) {
   std::string decoded_key;
-  boost::beast::detail::base64::decode((void*)&decoded_key, (const char*)&key, key.size());
+  decoded_key.resize(boost::beast::detail::base64::decoded_size(key.size()));
+  decoded_key.resize(boost::beast::detail::base64::decode((void*)&decoded_key, (const char*)&key, key.size()).first);
 
   std::shared_ptr<EVP_CIPHER_CTX> ctx{EVP_CIPHER_CTX_new(), ::EVP_CIPHER_CTX_free};
   if (not ctx) {
